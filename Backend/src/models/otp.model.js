9fee38b67b1e83300a8ab391382d1ec8d791262d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-22 22:34:03 +0530

    Add email verification functionality

--- Backend/src/models/otp.model.js
+++ Backend/src/models/otp.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { mailSender } from "../utils/mailSender.js";
 
 const otpSchema = new mongoose.Schema(
     {
@@ -20,6 +21,25 @@ const otpSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+// TODO: function to send Email
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = await mailSender(
+            email,
+            "Verification email from ed-Tech",
+            otp
+        );
+        console.log(`Verification mail send Successfully, ${mailResponse}`);
+    } catch (error) {
+        console.log(`ERR while sending verification Email: ${error}`);
+    }
+}
+
+otpSchema.pre("save", async function (next) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+});
 
 const Otp = mongoose.model("Otp", otpSchema);
 export { Otp };
