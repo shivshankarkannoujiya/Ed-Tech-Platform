aba33fda691f962f3bdbf32d36207f8d361a8bf8
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-22 14:40:51 +0530

    Add jsonwebtoken and cookie-parser dependencies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: false, limit: "16kb" }));
+app.use(cookieParser());
 
 // import router
 
