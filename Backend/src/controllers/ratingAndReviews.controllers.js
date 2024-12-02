90a176b4316c9aa542a8f43d019252203db1b4dd
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-02 22:43:41 +0530

    Add average rating calculation and fetch all ratings functionality

--- Backend/src/controllers/ratingAndReviews.controllers.js
+++ Backend/src/controllers/ratingAndReviews.controllers.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { APiResponse } from "../utils/ApiResponse.js";
 import { Course } from "../models/Course.model.js";
 import { RatingAndReview } from "../models/ratingAndReview.model.js";
+import mongoose from "mongoose";
 
 /**
  * Creating Rating 
@@ -58,7 +59,7 @@ const createRating = asyncHandler(async (req, res) => {
     })
 
     // update in Course
-    await Course.findByIdAndUpdate({ _id: courseId },
+    const updatedCourseDetails = await Course.findByIdAndUpdate({ _id: courseId },
         {
             $push: {
                 ratingAndReviews: ratingReview._id
@@ -67,8 +68,87 @@ const createRating = asyncHandler(async (req, res) => {
         { new: true }
     )
 
+    console.log(updatedCourseDetails);
+
+
+    return res
+        .status(200)
+        .json(
+            new APiResponse(
+                200,
+                ratingReview,
+                "Rating and Review Created Successfully"
+            )
+        )
 
 })
 
+const getAverageRating = asyncHandler(async (req, res) => {
+    /**
+     * get Course Id
+     * Calculate Avg Rating 
+     * Return Rating 
+     */
 
-export { createRating }
+    const { courseId } = req.body
+
+    const result = await RatingAndReview.aggregate([
+        {
+            $match: {
+                course: new mongoose.Types.ObjectId(courseId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                averageRating: { $avg: "$rating" }
+            }
+        }
+    ])
+
+    if (result.length > 0) {
+        return res
+            .status(200)
+            .json(
+                200,
+                {
+                    averageRating: result[0].averageRating
+                }
+            )
+    }
+
+    // if not rating/review Exist
+    return res
+        .status(200)
+        .json(
+            200,
+            {
+                averageRating: 0,
+            },
+            "Average Rating is 0, No rating given till now"
+        )
+})
+
+const getAllRating = asyncHandler(async (_, res) => {
+    const allRating = await RatingAndReview.find({})
+        .sort({ rating: "desc" })
+        .populate({
+            path: "user",
+            select: "firstName lastName email image"
+        })
+        .populate({
+            path: "course",
+            select: "courseName"
+        })
+        .exec()
+
+    return res
+        .status(200)
+        .json(
+            200,
+            allRating,
+            "all Rating and reviews fetched Successfully"
+        )
+})
+
+export { createRating, getAverageRating, getAllRating }
